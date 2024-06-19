4ba9e6b783f5bf94786039ea33bbacb3f1b41fe5
martinfyic <martin.f.yic@gmail.com>
2024-06-19 20:23:20 -0300

    refactor: :fire: remove update-orders.dto.ts

--- src/orders/dto/update-order.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrderDto } from './create-order.dto';
-
-// TODO: ver si lo usamos
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
