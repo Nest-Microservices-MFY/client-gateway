5490f037adf4becea468bd9b8cddf1fb982dda56
martinfyic <martin.f.yic@gmail.com>
2024-06-19 21:45:24 -0300

    refactor: :wrench: config nats vars

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,23 +3,21 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
-  PRODUCT_MICROSERVICE_HOST: string;
-  PRODUCT_MICROSERVICE_PORT: number;
-  ORDER_MICROSERVICE_HOST: string;
-  ORDER_MICROSERVICE_PORT: number;
+
+  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCT_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCT_MICROSERVICE_PORT: joi.number().required(),
-    ORDER_MICROSERVICE_HOST: joi.string().required(),
-    ORDER_MICROSERVICE_PORT: joi.number().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envsSchema.validate(process.env);
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation: ${error.message}`);
@@ -29,8 +27,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  productsMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
-  productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
-  ordersMicroserviceHost: envVars.ORDER_MICROSERVICE_HOST,
-  ordersMicroservicePort: envVars.ORDER_MICROSERVICE_PORT,
+  natsServers: envVars.NATS_SERVERS,
 };
